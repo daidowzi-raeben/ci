1ad314c16d17ef55d92c660a1300026cf236027b
daidowzi-raeben <45227695+daidowzi-raeben@users.noreply.github.com>
2020-06-14 10:58:23 +0900

    raeben

--- application/views/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-
-<div class="warp">
-    welcome to CI
-    css compile ????
-</div>
--- application/views/welcome_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<link rel="stylesheet" type="text/css" href="../css/common.css">
-
-<div class="warp">
-    welcome to CI
-    css compile ????
-</div>
